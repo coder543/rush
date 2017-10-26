0da85c89d80b6f9f08342b895af0a69647aa292e
Josh Leverette <josh.leverette@utiliflex.com>
2017-10-26 13:18:58 -0400

    Cleanup the error handling a little bit

--- src/errext.rs
+++ src/errext.rs
@@ -4,6 +4,7 @@ pub type PResult = Result<(), &'static str>;
 
 pub trait ErrExt {
     fn as_err(&self) -> PResult;
+    fn unwrap(&self);
 }
 
 impl ErrExt for i32 {
@@ -13,5 +14,9 @@ impl ErrExt for i32 {
         } else {
             Ok(())
         }
+    }
+
+    fn unwrap(&self) {
+        assert_ne!(*self, pancurses::ERR, "screen is broken?");
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,38 +2,40 @@ extern crate pancurses;
 use pancurses::{Input, Window};
 
 mod errext;
-use errext::ErrExt;
+use errext::{ErrExt, PResult};
 
-fn print_buffer(screen: &Window, buffer: &str) {
+fn print_buffer(screen: &Window, buffer: &str) -> PResult {
     let height = (screen.get_max_y() - 1) as usize;
     let width = screen.get_max_x() as usize;
-    screen.mv(0, 0).as_err().unwrap();
+    screen.mv(0, 0).as_err()?;
     for line in buffer.lines().take(height) {
         let max = ::std::cmp::min(width, line.len());
-        screen.addstr(&line[..max]).as_err().unwrap();
-        screen.addch('\n');
+        screen.addstr(&line[..max]).as_err()?;
+        screen.addch('\n').as_err()?;
     }
+    Ok(())
 }
 
-fn print_command(screen: &Window, buffer: &str) {
+fn print_command(screen: &Window, buffer: &str) -> PResult {
     let height = screen.get_max_y() - 1;
     let width = screen.get_max_x() - 1;
-    screen.mv(height - 1, 0).as_err().unwrap();
-    screen.hline(0, width).as_err().unwrap();
-    screen.mv(height, 0).as_err().unwrap();
+    screen.mv(height - 1, 0).as_err()?;
+    screen.hline(0, width).as_err()?;
+    screen.mv(height, 0).as_err()?;
     let max = ::std::cmp::min(width as usize, buffer.len());
-    screen.addstr(&buffer[..max]).as_err().unwrap();
+    screen.addstr(&buffer[..max]).as_err()?;
+    Ok(())
 }
 
-fn input_loop(screen: Window) {
+fn input_loop(screen: Window) -> PResult {
     let mut output_buffer = String::from("this is a test\nwith line breaks. ");
     let mut command_buffer = String::new();
     // let (mut x, mut y) = (0, 0);
     loop {
-        screen.clear().as_err().unwrap();
-        print_buffer(&screen, &output_buffer);
-        print_command(&screen, &command_buffer);
-        screen.refresh().as_err().unwrap();
+        screen.clear().as_err()?;
+        print_buffer(&screen, &output_buffer)?;
+        print_command(&screen, &command_buffer)?;
+        screen.refresh().as_err()?;
         let next = screen.getch().unwrap();
         match next {
             Input::KeyBackspace |
@@ -58,7 +60,7 @@ pub fn interactive() {
     pancurses::noecho();
     pancurses::set_title("rush");
     screen.keypad(true);
-    input_loop(screen);
+    input_loop(screen).unwrap();
 }
 
 #[cfg(test)]
