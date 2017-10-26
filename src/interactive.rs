585d6622c1e657ae63618b67868fca06f5104162
Josh Leverette <josh.leverette@utiliflex.com>
2017-10-26 13:54:35 -0400

    Make error handling panic at the site of the error

--- src/interactive.rs
+++ src/interactive.rs
@@ -5,11 +5,11 @@ use command::run_expression;
 fn print_buffer(screen: &Window, buffer: &str) -> PResult {
     let height = (screen.get_max_y() - 1) as usize;
     let width = screen.get_max_x() as usize;
-    screen.mv(0, 0).as_err()?;
+    screen.mv(0, 0).unwrap();
     for line in buffer.lines().take(height) {
         let max = ::std::cmp::min(width, line.len());
-        screen.addstr(&line[..max]).as_err()?;
-        screen.addch('\n').as_err()?;
+        screen.addstr(&line[..max]).unwrap();
+        screen.addch('\n').unwrap();
     }
     Ok(())
 }
@@ -17,11 +17,11 @@ fn print_buffer(screen: &Window, buffer: &str) -> PResult {
 fn print_command(screen: &Window, buffer: &str) -> PResult {
     let height = screen.get_max_y() - 1;
     let width = screen.get_max_x() - 1;
-    screen.mv(height - 1, 0).as_err()?;
-    screen.hline(0, width).as_err()?;
-    screen.mv(height, 0).as_err()?;
+    screen.mv(height - 1, 0).unwrap();
+    screen.hline(0, width).unwrap();
+    screen.mv(height, 0).unwrap();
     let max = ::std::cmp::min(width as usize, buffer.len());
-    screen.addstr(&buffer[..max]).as_err()?;
+    screen.addstr(&buffer[..max]).unwrap();
     Ok(())
 }
 
@@ -30,10 +30,10 @@ pub fn input_loop(screen: Window) -> PResult {
     let mut command_buffer = String::new();
     // let (mut x, mut y) = (0, 0);
     loop {
-        screen.clear().as_err()?;
+        screen.clear().unwrap();
         print_buffer(&screen, &output_buffer)?;
         print_command(&screen, &command_buffer)?;
-        screen.refresh().as_err()?;
+        screen.refresh().unwrap();
         let next = screen.getch().unwrap();
         match next {
             Input::KeyBackspace |
