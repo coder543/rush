5146248afb756c1de68b0939729470a2386994b6
Josh Leverette <josh.leverette@utiliflex.com>
2017-10-26 16:14:53 -0400

    Extend the AST

--- src/interpreter/ast.rs
+++ src/interpreter/ast.rs
@@ -9,7 +9,10 @@ pub enum Node {
     Ident(Ident),
     Int(i64),
     Float(f64),
+    Str(String),
     Op(Box<Operator>),
+    For(Box<For>),
+    While(Box<While>),
     Function(Box<Function>),
 }
 
@@ -30,7 +33,18 @@ pub enum Operator {
 }
 
 pub struct Function {
-    name: String,
+    name: Ident,
     args: Vec<Ident>,
+    body: Expr
+}
+
+pub struct For {
+    loopvar: Ident,
+    iterator: Expr,
+    body: Expr
+}
+
+pub struct While {
+    condition: Expr,
     body: Expr
 }
